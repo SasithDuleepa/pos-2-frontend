159d8a9fc27b71fbaf03ab18263d119b5e634122
SasithDuleepa <duleepajayathissa@gmail.com>
2023-09-26 13:30:49 +0530

    customer and supplier

--- src/pages/customer/customer.jsx
+++ src/pages/customer/customer.jsx
@@ -1,7 +1,92 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './customer.css';
+import axios from 'axios';
+import Edite from './../../components/icons/edit.png';
+import Delete from './../../components/icons/delete.png';
+
 
 export default function Customer() {
+    const[addbtn,setAddbtn] = useState('customer-add-button')
+    const[updatebtn,setUpdatebtn] = useState('deactive')
+    const [data,setdata] = useState({
+        customer_id:"",
+        customer_name:'',
+        customer_address:'',
+        customer_contact:'',
+        customer_email:'',
+        customer_nic:''
+    })
+    const handlechange = (e) => {
+        setdata({...data,[e.target.id]:e.target.value})
+    }
+
+    //add
+    const addcustomer = async() => {
+        const res = await axios.post(`${process.env.REACT_APP_BACKEND_API}/customers/add`,data)
+        console.log(res.data)
+        if(res.data.status === 200 ){
+            alert('Customer Added Successfully')
+            getallcustomers();
+        }else if(res.data.status === 400){
+            alert('Customer Not Added')
+        }else if(res.data.status === 500){
+            alert('Server Error')
+        }else{
+            alert('Something went wrong')
+        }
+    }
+    
+    //get all customers
+    const[customers,setcustomers] = useState([])
+    const getallcustomers = async() => {
+        const res = await axios.get(`${process.env.REACT_APP_BACKEND_API}/customers/all`)
+        console.log(res.data)
+        if(res.data.status === 200 ){
+            setcustomers(res.data.data)
+        }        
+    }
+    useEffect(() => {
+        getallcustomers();
+    },[])
+    
+    // edite icon function
+    const editeicon = (id,name,address,contact,email,nic)=>(e) => {
+        console.log(id,name,address,contact,email,nic)
+        setdata({
+            customer_id:id,
+            customer_name:name,
+            customer_address:address,
+            customer_contact:contact,
+            customer_email:email,
+            customer_nic:nic})
+        setAddbtn('deactive')
+        setUpdatebtn('customer-update-button')
+    
+        
+    }
+    // delete icon function
+    const deleteicon = (id) => async(e) => {
+        const res = await axios.delete(`${process.env.REACT_APP_BACKEND_API}/customers/delete/?id=${id}`)
+        console.log(res.data)
+        if(res.data.status === 200 ){
+            alert('Customer Deleted Successfully')
+            getallcustomers();
+    }else if(res.data.status === 500){
+        alert('Server Error')
+    }
+}
+    //update handle
+    const updatecustomer = async() => {
+        const res = await axios.put(`${process.env.REACT_APP_BACKEND_API}/customers/update/?id=${data.customer_id}`,data);
+        console.log(res.data)
+        if(res.data.status === 200 ){
+            alert('Customer Updated Successfully')
+            getallcustomers();
+        }else if(res.data.status === 500){
+            alert('Server Error')
+        
+        }
+    }
   return (
     <div className='customer-parent'>
         <div className='customer-child-1'>
@@ -10,34 +95,80 @@ export default function Customer() {
             <div className='customer-form-div-main'>
                 <div className='customer-form-div'>
                     <label className='customer-form-label'>Customer Name</label>
-                    <input className='customer-form-input' type="text" />
+                    <input className='customer-form-input' type="text" id='customer_name' onChange={(e)=>handlechange(e)} value={data.customer_name}/>
                 </div>
                 <div className='customer-form-div'>
                     <label className='customer-form-label'>Customer Address</label>
-                    <input  className='supplier-form-input'  type="text" />
+                    <input  className='supplier-form-input'  type="text" id='customer_address' onChange={(e)=>handlechange(e)} value={data.customer_address}/>
                 </div>
                 <div className='customer-form-div'>
                     <label className='customer-form-label'>Customer Contact</label>
-                    <input  className='customer-form-input'  type="text" />
+                    <input  className='customer-form-input'  type="text" id='customer_contact' onChange={(e)=>handlechange(e)} value={data.customer_contact}/>
                 </div>
                 <div className='customer-form-div'>
                     <label className='customer-form-label'>Customer Email</label>
-                    <input  className='customer-form-input'  type="text" />
+                    <input  className='customer-form-input'  type="text" id='customer_email' onChange={(e)=>handlechange(e)} value={data.customer_email}/>
                 </div>
                 <div className='customer-form-div'>
                     <label className='customer-form-label'>Customer NIC</label>
-                    <input  className='customer-form-input'  type="text" />
+                    <input  className='customer-form-input'  type="text" id='customer_nic' onChange={(e)=>handlechange(e)} value={data.customer_nic}/>
                 </div>
             </div>
             <div className='customer-add-button-div'>
-                <button className='customer-add-button'>Add</button>
-                {/* <button>Update</button> */}
+                <button className={addbtn} onClick={addcustomer}>Add</button>
+                <button className={updatebtn} onClick={updatecustomer}>Update</button>
             </div>
             </div>
             
             
         </div>
-        <div className='customer-child-2'></div>
+        <div className='customer-child-2'>
+            <div >
+                <div className='customer-view-div-header'>
+                    <p className='customer-name'>name</p>
+                    <p className='customer-address'>address</p>
+                    <p className='customer-contact'>contact</p>
+                    <p className='customer-email'>email</p>
+                    <p className='customer-nic'>nic</p>
+                    <p className='customer-action'>action</p>
+                </div>
+                {/* <div className='customer-view-div-value'>
+                    <p className='customer-name-value'>name</p>
+                    <p className='customer-address-value'>address</p>
+                    <p className='customer-contact-value'>contact</p>
+                    <p className='customer-email-value'>email</p>
+                    <p className='customer-nic-value'>nic</p>
+                    <div className='customer-action-value'>
+                        <img className='edite-img' src={Edite} alt="" />
+                        <img className='delete-img' src={Delete} alt="" />
+                    </div>
+                </div> */}
+                {customers.map((customer,index) => {
+                    return(
+                        <div className='customer-view-div-value' key={index}>
+                            <p className='customer-name-value'>{customer.customer_name}</p>
+                            <p className='customer-address-value'>{customer.customer_address}</p>
+                            <p className='customer-contact-value'>{customer.customer_contact}</p>
+                            <p className='customer-email-value'>{customer.customer_email}</p>
+                            <p className='customer-nic-value'>{customer.customer_nic}</p>
+                            <div className='customer-action-value'>
+                                <img className='edite-img' onClick={editeicon(
+                                    customer.customer_id,
+                                    customer.customer_name,
+                                    customer.customer_address,
+                                    customer.customer_contact,
+                                    customer.customer_email,
+                                    customer.customer_nic
+
+                                    )} src={Edite} alt="" />
+                                <img className='delete-img' onClick={deleteicon(customer.customer_id)} src={Delete} alt="" />
+                            </div>
+                        </div>
+                    )
+                }
+                )}
+            </div>
+        </div>
     </div>
   )
 }
--- src/pages/supplier/supplier.jsx
+++ src/pages/supplier/supplier.jsx
@@ -1,7 +1,95 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './supplier.css';
+import axios from 'axios';
+import Edite from './../../components/icons/edit.png';
+import Delete from './../../components/icons/delete.png';
 
 export default function Supplier() {
+    const[addbtn,setAddbtn] = useState('supplier-add-button')
+    const[updatebtn,setUpdatebtn] = useState('deactive')
+
+
+    const [data,setData] = useState({
+        supplier_id:"",
+        supplier_name:'',
+        supplier_address:'',
+        supplier_contact:'',
+        supplier_email:'',
+        supplier_nic:''
+    })
+    const handleInputChange = (e) => {
+        setData({...data,[e.target.id]:e.target.value})
+    }
+
+    //add
+    const addSupplier = async() => {
+        const res = await axios.post(`${process.env.REACT_APP_BACKEND_API}/suppliers/add`,data)
+        console.log(res.data)
+        if(res.data.status === 200 ){
+            alert('Supplier Added Successfully')
+            
+        }else if(res.data.status === 500){
+            alert('Server Error')
+        
+        }else if(res.data.status === 400){
+            alert('Supplier Not Added')
+        }else{
+            alert('Something went wrong')
+        
+        }
+    
+    }
+
+    //get all suppliers
+    const[suppliers,setsuppliers] = useState([])
+    const getAllSuppliers = async() => {
+        const res = await axios.get(`${process.env.REACT_APP_BACKEND_API}/suppliers/all`)
+        console.log(res.data)
+        setsuppliers(res.data.data)
+    }
+    useEffect(() => {
+        getAllSuppliers();
+    
+    },[])
+
+
+    //edite icon
+    const editeicon = (id,name,address,contact,email,nic) => (e) => {
+        setData({supplier_id:id, supplier_name:name,supplier_address:address,supplier_contact:contact,supplier_email:email,supplier_nic:nic})
+        setAddbtn('deactive')
+        setUpdatebtn('supplier-update-button')
+    
+    }
+    //update 
+    const updateSupplier = async() => {
+        const res = await axios.put(`${process.env.REACT_APP_BACKEND_API}/suppliers/update/?id=${data.supplier_id}`,data);
+        console.log(res.data)
+        if(res.data.status === 200 ){
+            alert('Supplier Updated Successfully')
+            getAllSuppliers();
+        }else if(res.data.status === 500){
+            alert('Server Error')
+        }else{
+            alert('Supplier Not Updated')
+        }
+        
+    }
+
+    //delete
+    const deleteSupplier = (id) => async(e) =>  {
+        const res = await axios.delete(`${process.env.REACT_APP_BACKEND_API}/suppliers/delete/?id=${id}`);
+        console.log(res.data)
+        if(res.data.status === 200 ){
+            alert('Supplier Deleted Successfully')
+            getAllSuppliers();
+        }else if(res.data.status === 500){
+            alert('Server Error')
+        }else{
+            alert('Supplier Not Deleted')
+        }
+        
+    
+    }
   return (
     <div className='supplier-parent'>
         <div className='supplier-child-1'>
@@ -10,34 +98,71 @@ export default function Supplier() {
             <div className='supplier-form-div-main'>
                 <div className='supplier-form-div'>
                     <label className='supplier-form-label'>Supplier Name</label>
-                    <input className='supplier-form-input' type="text" />
+                    <input className='supplier-form-input' id='supplier_name' value={data.supplier_name} onChange={(e)=>handleInputChange(e)} type="text" />
                 </div>
                 <div className='supplier-form-div'>
                     <label className='supplier-form-label'>Supplier Address</label>
-                    <input  className='supplier-form-input'  type="text" />
+                    <input  className='supplier-form-input' id='supplier_address' value={data.supplier_address}  onChange={(e)=>handleInputChange(e)}  type="text" />
                 </div>
                 <div className='supplier-form-div'>
                     <label className='supplier-form-label'>Supplier Contact</label>
-                    <input  className='supplier-form-input'  type="text" />
+                    <input  className='supplier-form-input' id='supplier_contact' value={data.supplier_contact} onChange={(e)=>handleInputChange(e)}  type="text" />
                 </div>
                 <div className='supplier-form-div'>
                     <label className='supplier-form-label'>Supplier Email</label>
-                    <input  className='supplier-form-input'  type="text" />
+                    <input  className='supplier-form-input' id='supplier_email' value={data.supplier_email}   onChange={(e)=>handleInputChange(e)}   type="text" />
                 </div>
                 <div className='supplier-form-div'>
                     <label className='supplier-form-label'>Supplier NIC</label>
-                    <input  className='supplier-form-input'  type="text" />
+                    <input  className='supplier-form-input' id='supplier_nic' value={data.supplier_nic}   onChange={(e)=>handleInputChange(e)}  type="text" />
                 </div>
             </div>
             <div className='supplier-add-button-div'>
-                <button className='supplier-add-button'>Add</button>
-                {/* <button>Update</button> */}
+                <button className={addbtn} onClick={addSupplier}>Add</button>
+                <button className={updatebtn}  onClick={updateSupplier}>Update</button>
             </div>
             </div>
             
             
         </div>
-        <div className='supplier-child-2'></div>
-    </div>
+        <div className='supplier-child-2'>
+            <div className='supplier-view-div'>
+                <div className='supplier-view-div-header'>
+                    <p className='supplier-name'>name</p>
+                    <p className='supplier-address'>address</p>
+                    <p className='supplier-contact'>contact</p>
+                    <p className='supplier-email'>email</p>
+                    <p className='supplier-nic'>nic</p>
+                    <p className='supplier-delete'>delete</p>
+                </div>
+                {suppliers.map((supplier,index) => {
+                    return(
+                        <div className='supplier-view-div-value' key={index}>
+                            <p className='supplier-name-value'>{supplier.supplier_name}</p>
+                            <p className='supplier-address-value'>{supplier.supplier_address}</p>
+                            <p className='supplier-contact-value'>{supplier.supplier_contact}</p>
+                            <p className='supplier-email-value'>{supplier.supplier_email}</p>
+                            <p className='supplier-nic-value'>{supplier.supplier_nic}</p>
+                            <div className='supplier-action-value'>
+                                <img className='edite-img' src={Edite} onClick={editeicon(
+                                    supplier.supplier_id,
+                                    supplier.supplier_name,
+                                    supplier.supplier_address,
+                                    supplier.supplier_contact,
+                                    supplier.supplier_email,
+                                    supplier.supplier_nic
+                                    )} alt="" />
+                                <img className='delete-img' onClick={deleteSupplier(supplier.supplier_id)} src={Delete} alt="" />
+                            </div>
+                        </div>
+                    )
+                }
+                )}
+
+                </div>
+            </div>
+        </div>
+
+
   )
 }
